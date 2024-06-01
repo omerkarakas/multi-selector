685be15e76da1243b47ec2cde6294138ccaab96f
omerkarakas <karakasomer@gmail.com>
2024-06-01 16:53:54 +0300

    A few fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,70 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import MultiSelect from "./components/MultiSelect";
+import { Character } from "./lib/schema";
+
+const API_URL = "https://rickandmortyapi.com/api/character";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFetching, setIsFetching] = useState(true);
+
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) return;
+    async function getData() {
+      const list: Character[] = [];
+      let data;
+      let nextUrl = API_URL;
+
+      try {
+        do {
+          const response = await fetch(nextUrl);
+          data = await response.json();
+          list.push(...data.results);
+          nextUrl = data.info.next;
+        } while (nextUrl);
+        setErrorMessage("");
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setErrorMessage(error.message);
+        }
+      }
+
+      setCharacters(list);
+    }
+    setIsFetching(true);
+    getData();
+    setIsFetching(false);
+  }, [isLoading]);
+
+  if (isLoading || isFetching) return <div>Loading...</div>;
+
   return (
     <div className="flex flex-col gap-6 p-8 max-w-[600px]">
       <h1 className="text-3xl">Rick And Morthy, MultiSelect Component Demo</h1>
-      <MultiSelect />
+      {errorMessage ? (
+        <div>{errorMessage}</div>
+      ) : (
+        <>
+          {characters.length === 0 ? (
+            <>
+              <div>Loading...</div>
+            </>
+          ) : (
+            <div className="flex flex-col gap-1">
+              <MultiSelect characters={characters} />
+              {errorMessage && <div>{errorMessage}</div>}
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import { Character } from "../types/schema";
+import { useRef, useState } from "react";
+import { Character } from "../lib/schema";
 import useDebounce from "../assets/hooks/useDebounce";
 import Highlighter from "react-highlight-words";
 
@@ -13,10 +13,12 @@ function Char<T extends Character>({
   return (
     <div className="flex flex-nowrap gap-1 bg-slate-200 rounded-xl px-2 py-1 items-center">
       <span>{char.name}</span>
-      <i
-        className="fa-solid fa-xmark text-sm text-white bg-slate-400 rounded-md px-2 py-1 h-7 w-7 ml-2"
+      <button
         onClick={removeCharacter}
-      />
+        className="px-2 py-1 h-7 w-7 rounded-md text-sm text-white bg-slate-400"
+      >
+        <i className="fa-solid fa-xmark  " />
+      </button>
     </div>
   );
 }
@@ -26,18 +28,20 @@ function CharWithImage<T extends Character>({
   selected,
   toggleCharacter,
   searchTerm,
+  className,
 }: {
   char: T;
   selected: boolean;
   toggleCharacter: () => void;
   searchTerm?: string;
+  className?: string;
 }) {
   return (
     <div
-      className="flex flex-nowrap text-base gap-3 border-b last:border-b-0 border-slate-400 p-2 items-center "
+      className={`flex flex-nowrap text-base gap-3 border-b last:border-b-0 border-slate-400 p-2 items-center ${className}`}
       onClick={toggleCharacter}
     >
-      <input type="checkbox" checked={selected} className="w-4 h-4" />
+      <input type="checkbox" checked={selected} readOnly className="w-4 h-4" />
       <img
         src={char.image}
         alt={char.name}
@@ -58,58 +62,90 @@ function CharWithImage<T extends Character>({
   );
 }
 
-function MultiSelect<T extends Character>() {
+function MultiSelect<T extends Character>({ characters }: { characters: T[] }) {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFetching, setIsFetching] = useState(false);
 
   const [searchTerm, setSearchTerm] = useState("");
 
   const [showResults, setShowResults] = useState(false);
 
-  const [characters, setCharacters] = useState<T[]>([]);
   const [filteredCharacters, setFilteredCharacters] = useState<T[]>([]);
 
   const [selectedCharacters, setSelectedCharacters] = useState<T[]>([]);
 
-  useEffect(() => {
-    async function getData() {
-      const list = [];
-      let data;
-      let nextUrl = "https://rickandmortyapi.com/api/character";
-      do {
-        // console.log("API call : " + nextUrl);
-        const response = await fetch(nextUrl);
-        data = await response.json();
-        list.push(...data.results);
-        nextUrl = data.info.next;
-      } while (nextUrl);
-      setCharacters(list);
-      // console.log("chars:", list);
-    }
-
-    getData();
-  }, []);
-
-  useEffect(() => {
-    setFilteredCharacters(
-      characters.filter((char) =>
-        char.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-  }, [searchTerm]);
+  const [cursor, setCursor] = useState(0);
 
   useDebounce(
     () => {
+      setIsFetching(true);
+      setFilteredCharacters(
+        characters.filter((char) =>
+          char.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      );
+      setIsFetching(false);
+      setCursor(0);
+
       setShowResults(!!searchTerm);
     },
-    200,
+    100,
     [searchTerm]
   );
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    const { key } = e;
+    console.log("cursor:", cursor);
+
+    if (key === "ArrowUp" && cursor > 0) {
+      setCursor((c) => c - 1);
+    } else if (key === "ArrowDown") {
+      setFilteredCharacters(
+        characters.filter((char) =>
+          char.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      );
+      setShowResults(true);
+      if (cursor < filteredCharacters.length - 1) setCursor((c) => c + 1);
+    } else if (key === "Tab" || key === "Escape") {
+      setShowResults(false);
+      setSearchTerm("");
+    } else if (
+      key === "Enter" &&
+      cursor <= filteredCharacters.length - 1 &&
+      cursor >= 0
+    ) {
+      if (selectedCharacters.includes(filteredCharacters[cursor])) {
+        setSelectedCharacters(
+          selectedCharacters.filter(
+            (char) => char !== filteredCharacters[cursor]
+          )
+        );
+      } else {
+        setSelectedCharacters([
+          ...selectedCharacters,
+          filteredCharacters[cursor],
+        ]);
+      }
+    }
+  }
+
+  if (isFetching || characters.length === 0) {
+    return (
+      <div className="flex flex-col items-stretch ">
+        <div className="flex flex-row justify-between items-start rounded-xl border border-slate-600 text-slate-800 p-2 my-2 shadow-md">
+          Loading...
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-stretch ">
       <div
         onClick={() => inputRef?.current?.focus()}
-        className="flex flex-row justify-between items-start  rounded-xl border border-slate-600 text-slate-800 p-2 my-2 shadow-md"
+        className="flex flex-row justify-between items-start rounded-xl border border-slate-600 text-slate-800 p-2 my-2 shadow-md"
       >
         <div className="flex flex-wrap gap-2 ">
           {selectedCharacters.map((d, i) => (
@@ -127,18 +163,24 @@ function MultiSelect<T extends Character>() {
             type="text"
             className="border-none focus:outline-none w-20 h-10"
             ref={inputRef}
+            onKeyDown={handleKeyDown}
+            value={searchTerm}
             onChange={(e) => {
-              console.log("e:", e.target.value);
               setSearchTerm(e.target.value);
-
-              // setShowResults(!!e.target.value);
             }}
           />
         </div>
         <div className="h-10 w-10 flex items-center justify-center">
           <i
             className="fa-solid fa-caret-down"
-            onClick={() => setShowResults((r) => !r)}
+            onClick={() => {
+              setFilteredCharacters(
+                characters.filter((char) =>
+                  char.name.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+              );
+              setShowResults((r) => !r);
+            }}
           ></i>
         </div>
       </div>
@@ -167,6 +209,9 @@ function MultiSelect<T extends Character>() {
                   }}
                   selected={selectedCharacters.includes(filteredCharacter)}
                   searchTerm={searchTerm}
+                  className={
+                    cursor === index ? "active scroll-mt-4" : undefined
+                  }
                 />
               );
             })}
